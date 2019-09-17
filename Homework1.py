40f1205095794ec18da9f6ead43d67eaf1d5ffb5
egilbertson-ucsf <erin.n.gilbertson@gmail.com>
2019-09-16 19:23:56 -0700

    fixed dumb things I had done to read in files

--- Homework1.py
+++ Homework1.py
@@ -1,41 +1,40 @@
-import os,sys, math
+import os,sys
+def read_exp_file(file):
+    file = open(file)
+    data = {}
+    cellCol = {}
+    cells = file.readline().replace('"','').strip('\n').split(',')
+    for line in file:
+        (gene,exp) = line.split(',',1)
+        data[gene] = exp.strip('\n').split(',')
+    for cell_i in range(len(cells)):
+        cellCol[cells[cell_i]] = cell_i
+    return data, cellCol
 
-def read_clusters(clusters):
-    file = open(clusters)
-    clustDict = {}
+def read_clust_file(file):
+    file = open(file)
+    clustCell = {}
     cells = file.readline().replace('"','').strip('\n').split(',')
     for line in file.readlines():
-        if line[0]:
-            for i in range(len(line.split(','))-1):
-                if i > 1:
-                    if line.split(',')[i] in clustDict:
-                        clustDict[cells[i]].append(line.split(',')[i])
+        line = line.replace('"','').strip('\n').split(',')
+        if line[0]=='TRUE':
+            for cell in range(len(line)):
+                if cell > 1:
+                    print(line[cell] in clustCell)
+                    if int(line[cell]) not in clustCell:
+                        clustCell[int(line[cell])] = [cells[cell]]
+                        print(clustCell[int(line[cell])])
+                        print([cells[cell]])
+                        print('added not in')
                     else:
-                        clustDict[cells[i]] = line.split(',')[i]
-    return clustDict
-
-def read_expression(exp):
-        file = open(exp)
-        expDict = {}
-        cells = file.readline().replace('"','').strip('\n').split(',')
-        for line in file.readlines():
-            for i in range(len(line.split(','))-1):
-                if i > 0:
-                    expDict[cells[i]] = i
-        return expDict
-
-def calc_clust_avg(clustDict, expDict, clustNum):
-    for value in clustDict[clustNum]:
-        
-    return
-
-
-
-
+                        clustCell[int(line[cell])].append(cells[cell])
+                        print('added else')
+    return clustCell
 
 def main():
     return
 if __name__ == '__main__':
-    cellClustDict = read_clusters(sys.argv[1])
-    cellColDict = read_expression(sys.argv[2])
+    expData, cellCol = read_exp_file(sys.argv[1])
+    clustCell = read_clust_file(sys.argv[2])
+    print(clustCell)
     main()
