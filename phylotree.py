ef899d919fa0df589b18a9711a785eaa0cddcf8d
egilbertson-ucsf <erin.n.gilbertson@gmail.com>
2019-09-27 10:41:14 -0700

    added docs

--- phylotree.py
+++ phylotree.py
@@ -4,6 +4,16 @@ import pandas as pd
 import seaborn as sns
 
 def plot_trees(true_tree, best_clust_tree):
+
+    '''
+    Function to print both a true tree and a tree based on clutering methods to the command line
+    The tree will print in ascii format
+
+    Input: two tree object
+    Product: Ascii formatted trees printed to terminal interface
+
+    no returned object
+    '''
     print('True Tree')
     bp.draw_ascii(true_tree)
     print('Best Clustering Based Tree')
@@ -12,6 +22,15 @@ def plot_trees(true_tree, best_clust_tree):
 
 
 def plot_dist(random, cluster):
+    '''
+    Function to boxplot the distributions of our clustering method (*100) scores
+    next to random clustering scores
+
+    Input: two lists of adjusted rand index scores 
+    Product: boxplot
+
+    no returned object
+    '''
     rand = pd.Series(random)
     clust = pd.Series(cluster)
     dist = pd.DataFrame(pd.concat([rand,clust], axis=1))
