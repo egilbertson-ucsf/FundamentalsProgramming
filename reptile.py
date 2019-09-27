f22414c153f8cbc7048c2e8cb874222371d96ebc
asnanda <scott.nanda@ucsf.edu>
2019-09-26 23:32:26 -0700

    Added --list option

--- reptile.py
+++ reptile.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 from cluster_grading import grade as cg
 from phylotree import plot_trees
@@ -14,7 +15,7 @@ def main():
     argument_parser=argparse.ArgumentParser()
 
     ## Add argument for clustering method
-    argument_parser.add_argument("method",type=str,nargs=1,metavar='method',
+    argument_parser.add_argument("method",type=str,nargs='?',metavar='method',default='ward',
                                  help="Specify an aggolmerative clustering method for reconstructing the phylogeny. Ex. 'ward'")
 
     ## Add argument for metrics
@@ -25,20 +26,34 @@ def main():
     argument_parser.add_argument("-b","--bootstraps", dest='n_bootstraps',default=100,
                                  help="Specify the number of bootstrap resamplings of the data matrix. Default is 100.")
 
+    
+    argument_parser.add_argument("-l","--list",action='store_true',dest='list_methods',
+                                 help="Lists available clustering methods / distance metrics")
     ## Parse arguments
     args = argument_parser.parse_args()
+    
+    if args.list_methods:
+        cobj = cl.Cluster()
+        print("Available Methods:")
+        print("    "+", ".join(cobj.available_methods))
+        print("Available Distance Metrics:")
+        print("    "+", ".join(cobj.available_metrics))
         
-    true_tree = ld.TrueTree().load_true_tree('data/phyliptree.phy')
-    data = ld.CNVData().readCNVMatrix('data/LS_blastn_Gar_noDenom.txt')
-   
-    trees, random_trees, clust_ids, random_clust_ids = cl.Cluster().cluster(data,args.method[0],args.metric,args.n_bootstraps)
+    else:
+        print("Loading data...")
+        true_tree = ld.TrueTree().load_true_tree('data/phyliptree.phy')
+        data = ld.CNVData().readCNVMatrix('data/LS_blastn_Gar_noDenom.txt')
 
-    ari_clusters, ari_random = cg(data.index, trees, random_trees, clust_ids,random_clust_ids,true_tree)
+        print("Clustering and reconstructing phylogeny...")
+        trees, random_trees, clust_ids, random_clust_ids = cl.Cluster().cluster(data,args.method[0],args.metric,args.n_bootstraps)
 
-    ari_clusters, ari_random = cg(data.index, trees, random_trees, clust_ids,random_clust_ids,true_tree)
-    ac=pd.Series(ari_clusters)
-    plt.plot_trees(true_tree, trees[ac.idxmax()])
-    plt.plot_dist(ari_random, ari_clusters)
+        print("Scoring clusters...")
+        ari_clusters, ari_random = cg(data.index, trees, random_trees, clust_ids,random_clust_ids,true_tree)
+
+        ari_clusters, ari_random = cg(data.index, trees, random_trees, clust_ids,random_clust_ids,true_tree)
+        ac=pd.Series(ari_clusters)
+        plt.plot_trees(true_tree, trees[ac.idxmax()])
+        plt.plot_dist(ari_random, ari_clusters)
 
     exit()
     return
