9b9406c600ee6a696d9b0e36b50ea48209ab9dfe
asnanda <scott.nanda@ucsf.edu>
2019-09-26 23:11:29 -0700

    Added reptile.py

---
+++ reptile.py
@@ -0,0 +1,47 @@
+import argparse
+from cluster_grading import grade as cg
+from phylotree import plot_trees
+import load_data as ld
+import cluster as cl
+import phylotree as plt
+import scipy.cluster as sc
+from io import StringIO
+import numpy as np
+import Bio.Phylo as bp
+import pandas as pd
+
+def main():
+    argument_parser=argparse.ArgumentParser()
+
+    ## Add argument for clustering method
+    argument_parser.add_argument("method",type=str,nargs=1,metavar='method',
+                                 help="Specify an aggolmerative clustering method for reconstructing the phylogeny. Ex. 'ward'")
+
+    ## Add argument for metrics
+    argument_parser.add_argument("-m","--metric",dest='metric',default='euclidean',
+                                 help="Specify the distance metric used. Ex. 'euclidean'")
+
+    ## Add argument for bootstraps
+    argument_parser.add_argument("-b","--bootstraps", dest='n_bootstraps',default=100,
+                                 help="Specify the number of bootstrap resamplings of the data matrix. Default is 100.")
+
+    ## Parse arguments
+    args = argument_parser.parse_args()
+        
+    true_tree = ld.TrueTree().load_true_tree('data/phyliptree.phy')
+    data = ld.CNVData().readCNVMatrix('data/LS_blastn_Gar_noDenom.txt')
+   
+    trees, random_trees, clust_ids, random_clust_ids = cl.Cluster().cluster(data,args.method[0],args.metric,args.n_bootstraps)
+
+    ari_clusters, ari_random = cg(data.index, trees, random_trees, clust_ids,random_clust_ids,true_tree)
+
+    ari_clusters, ari_random = cg(data.index, trees, random_trees, clust_ids,random_clust_ids,true_tree)
+    ac=pd.Series(ari_clusters)
+    plt.plot_trees(true_tree, trees[ac.idxmax()])
+    plt.plot_dist(ari_random, ari_clusters)
+
+    exit()
+    return
+  
+if __name__ == '__main__':
+    main()
